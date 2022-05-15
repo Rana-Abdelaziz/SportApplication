6528173bea79eed36bc77027221252c9e9731584
Rana-Abdelaziz <61909090+Rana-Abdelaziz@users.noreply.github.com>
2022-05-15 14:40:54 -0700

    team screen

--- SportApplication/Models/TeamViewController.swift
+++ SportApplication/Models/TeamViewController.swift
@@ -12,16 +12,36 @@ class TeamViewController: UIViewController {
     
     let presnter :TeamsPresnterProtocol = TeamPresenter()
     
-    @IBOutlet weak var myLable: UILabel!
+    @IBOutlet weak var teamTshirtImg: UIImageView!
+    @IBOutlet weak var backgroundImg: UIImageView!
+    @IBOutlet weak var teamImg: UIImageView!
+    @IBOutlet weak var teamName: UILabel!
+    @IBOutlet weak var countryName: UILabel!
+    @IBOutlet weak var stadiumName: UILabel!
+    @IBOutlet weak var leagueName: UILabel!
+    @IBAction func instagramBtn(_ sender: Any) {
+        
+    }
+    @IBAction func websiteBtn(_ sender: Any) {
+        
+    }
+    @IBAction func twitterBtn(_ sender: Any) {
+        
+    }
+    @IBAction func faceBookBtn(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         presnter.getTeams(url: "https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=English%20Premier%20League") { (TeamsResualt, Error) in
             guard let r = TeamsResualt else{
                 print(Error)
-                self.myLable.text = Error as! String
+              //  self.myLable.text = Error as! String
                 return
             }
-            self.myLable.text = r.teams![0].strDescriptionEN
+           // self.myLable.text = r.teams![0].strDescriptionEN
             print(r.teams)
         }
         // Do any additional setup after loading the view.
